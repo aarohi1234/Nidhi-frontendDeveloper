a82639a418d3c24897d80e2ad0e544b9c701a814
Nidhi kumari <nidhiaarohi123@gmail.com>
2023-07-11 21:46:25 +0530

    final

--- myapp/src/components/Footer.js
+++ myapp/src/components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Footer() {
     return (
-        <section className='footerContainer px-5 py-5'>
+        <section className='fheaderContainer absolute flex items-center justify-between px-5 w-full'>
             <h2 className='mb-5 font-bold text-3xl'>SpaceX</h2>
             <div>
-                <ul className='flex flex-col gap-5 px-8 '>
+                <ul className='flex flex-row items-center justify-evenly gap-5'>
                     <li className="cursor-pointer"><a href="#Homepage">Homepage</a></li>
                     <li className="cursor-pointer"><a href="#Search">Search</a></li>
                     <li className="cursor-pointer"><a href="#capsules">Capsules</a></li>
