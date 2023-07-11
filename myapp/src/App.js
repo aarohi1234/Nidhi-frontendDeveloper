b34e6090c7f43891ad9e4e299741da8847c52090
Nidhi kumari <nidhiaarohi123@gmail.com>
2023-07-11 21:34:51 +0530

    commits

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import { Homepage, Capsules, Search } from './pages'
+import { Navbar, Footer } from './components'
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState("")
+  const [filterType, setFilterType] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Homepage />
+      <Search setValue={setValue} setFilterType={setFilterType} filterType={filterType} />
+      <Capsules filterType={filterType} value={value} />
+      <Footer />
+    </>
   );
 }
 
